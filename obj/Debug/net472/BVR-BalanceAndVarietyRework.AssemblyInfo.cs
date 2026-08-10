b06188ac4d0f286d018b553e3fcad3724c2a2888
Draken0015 <Discord - drakethewabbit>
2026-08-11 01:09:11 +0300

    Branch Creation

--- obj/Debug/net472/BVR-BalanceAndVarietyRework.AssemblyInfo.cs
+++ obj/Debug/net472/BVR-BalanceAndVarietyRework.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Nuclear Option - Balance and Variety Rework")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.7.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.7+c9a8ef2811932292ee9d7680b9bac4ab7a550e5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.7+afcc2a3ca73a64fe8c188c79a2d556ea073f543e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BVR-BalanceAndVarietyRework-Dev")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BVR-BalanceAndVarietyRework-Dev")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.7.0")]
